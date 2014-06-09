fc98fbe4cc027d7d853538311199fa1e74e89009
andre biester <andre.biester@stud.fh-luebeck.de>
2014-06-09 16:38:43 +0200

    Start 2

---
+++ WumpusDart/packages/WumpauDart/wumpuswelt.dart
@@ -0,0 +1,10 @@
+library wumpuswelt;
+
+import 'dart:html';
+import 'dart:io';
+
+part 'src/model.dart';
+part 'src/controller.dart';
+part 'src/view.dart';
+
+const level = 1;
---
+++ WumpusDart/web/wumpus.dart
@@ -0,0 +1,4 @@
+import 'package:WebTechProj/wumpuswelt.dart';
+
+
+main() => new WumpusView();
