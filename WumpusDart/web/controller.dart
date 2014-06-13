43737e8631e2adfd7bc4e08f25d553b9dd47876a
Daniel <d.hildenbeutel@googlemail.com>
2014-06-13 15:29:54 +0200

    test

--- WumpusDart/web/controller.dart
+++ WumpusDart/web/controller.dart
@@ -1,16 +1,16 @@
 import 'dart:html';
 import 'model.dart';
 
-class WumpusController  {
+class WumpusController {
   /**
    * Präsentiert das Model
    */
-  var game = new WumpusWelt(0,0);
+  var game = new WumpusWelt(0, 0);
   /**
    * Präsentiert die View
    */
   final view = new WumpusView();
-  
+
   /**
    * Konstruktor der Wumpuswelt
    * 
@@ -18,36 +18,44 @@ class WumpusController  {
   WumpusController() {
     view.startButton.onClick.listen((_) {
       game.lvl = game.lvl + 1;
-      switch(game.lvl)  {
+      switch (game.lvl) {
         case 1:
-           game = new WumpusSpiel(8,1);
-           break;
+          game = new WumpusSpiel(8, 1);
+          break;
         case 2:
-           game = new WumpusSpiel(9,2);
-           break;
+          game = new WumpusSpiel(9, 2);
+          break;
       }
       view.generateField(game);
       game.ingame = true;
       view.update(game);
     });
-    
-    
+
+
     // Steuerung des Spielers
     window.onKeyDown.listen((KeyboardEvent ev) {
       if (game.ingame) {
-      switch (ev.keyCode) {
-        case KeyCode.LEFT: game.wumpus.headLeft(); break;
-        case KeyCode.RIGHT: game.wumpus.headRight(); break;
-        case KeyCode.UP: game.wumpus.headUp(); break;
-        case KeyCode.DOWN: game.wumpus.headDown(); break;
+        switch (ev.keyCode) {
+          case KeyCode.LEFT:
+            game.wumpus.headLeft();
+            break;
+          case KeyCode.RIGHT:
+            game.wumpus.headRight();
+            break;
+          case KeyCode.UP:
+            game.wumpus.headUp();
+            break;
+          case KeyCode.DOWN:
+            game.wumpus.headDown();
+            break;
+        }
+        bewegung();
       }
-      bewegung();
-     }
     });
   }
-  
+
   void bewegung() {
-    if(game.keineWand)  {
+    if (game.keineWand) {
       view.upate(game);
     }
   }
