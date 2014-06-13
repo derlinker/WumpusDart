785f8d9dabc3e3e9055162a96479f6882b8502cf
andre biester <andre.biester@stud.fh-luebeck.de>
2014-06-13 15:01:33 +0200

    Controller 1.4

--- WumpusDart/web/controller.dart
+++ WumpusDart/web/controller.dart
@@ -1,8 +1,11 @@
+import 'dart:html';
+import 'model.dart';
+
 class WumpusController  {
   /**
    * Präsentiert das Model
    */
-  var game = new WumpusWelt(gamesize);
+  var game = new WumpusWelt(0,0);
   /**
    * Präsentiert die View
    */
@@ -14,20 +17,38 @@ class WumpusController  {
    */
   WumpusController() {
     view.startButton.onClick.listen((_) {
-      game = new WumpusWelt(level);
+      game.lvl = game.lvl + 1;
+      switch(game.lvl)  {
+        case 1:
+           game = new WumpusSpiel(8,1);
+           break;
+        case 2:
+           game = new WumpusSpiel(9,2);
+           break;
+      }
       view.generateField(game);
-      game.start();
+      game.ingame = true;
       view.update(game);
     });
+    
+    
     // Steuerung des Spielers
     windows.onKeyDown.listen((KeyboardEvent ev) {
-      if (game.finish) return;
+      if (game.ingame) {
       switch (ev.keyCode) {
         case KeyCode.LEFT: game.wumpus.headLeft(); break;
         case KeyCode.RIGHT: game.wumpus.headRight(); break;
         case KeyCode.UP: game.wumpus.headUp(); break;
         case KeyCode.DOWN: game.wumpus.headDown(); break;
       }
+      bewegung();
+     }
     });
+  }
+  
+  void bewegung() {
+    if(game.keineWand)  {
+      view.upate(game);
+    }
   }
 }
