02c4bac2d98069e97f503a38895a56316dccad69
Helge Hintze <helge.tobias.hintze@stud-fh-luebeck.de>
2014-06-29 13:21:28 +0200

    View Bug fix

--- WumpusDart/web/view.dart
+++ WumpusDart/web/view.dart
@@ -93,7 +93,7 @@ class WumpusView {
       for(int col = 0; col < field[row].length; col++) {
         final assignment = field[row][col];
         final position = "field_${row}_${col}";
-        table += "<td id = $position' class =$assignment'> row: $row col: $col typ: $assignment  </td>";
+        table += "<td id = $position' class =$assignment> row: $row col: $col typ: $assignment  </td>";
       }
       table+= "</tr>";
     }
